b015b2f75cc2f07e4e49677459972a8fda84bff1
Erik Barroso <erik.barroso@primary.health>
2022-07-08 14:31:32 -0400

    adds state postal code lookup, and allows for flexible state param

--- lib/fips_code_lookup.rb
+++ lib/fips_code_lookup.rb
@@ -5,12 +5,39 @@ require_relative "fips_code_lookup/version"
 module FipsCodeLookup
   class Error < StandardError; end
 
-  # Your code goes here...
-  def self.county(state, name)
-    # lookup state code to file
-    # parse file for name and return fips code
-    csv = CSV.read("county/#{state}.csv", headers: true)
-    code_row = csv.find { |row| row["NAME"] == name }
-    puts "ROW: ", code_row
+  STATE_POSTAL_CODES = %w[AL AK AZ AR CA CO CT DE DC FL GA HI ID IL IN IA KS
+                          KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC
+                          ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY
+                          AS GU MP PR UM VI].freeze
+
+  def self.county(state_param, name)
+    # Headers — AL,01,001,Autauga County,H1
+    File.foreach(county_filename(state_param)) do |row|
+      state_pc, state_fips, county_fips, county, class_code = row.split(",")
+      if county == name
+        return { "county_fips": county_fips, "state_fips": state_fips,
+                 "fips_class": class_code.gsub(/\n/, "") }
+      end
+    end
+  end
+
+  def self.state_postal_code(state)
+    state_file = File.join(File.dirname(__FILE__), "/state.csv")
+    # HEADERS — 01,AL,Alabama, 01779775
+    File.foreach(state_file) do |row|
+      state_fips, state_pc, state_name, geo_id = row.split(",")
+      if state == state_fips || state.upcase == state_pc || state.upcase == state_name.upcase || state == geo_id.gsub(/\n/, "")
+        return state_pc
+      end
+    end
+  end
+
+  def self.county_filename(state)
+    state_code = if STATE_POSTAL_CODES.include?(state.upcase)
+                   state.upcase
+                 else
+                   state_postal_code(state)
+                 end
+    File.join(File.dirname(__FILE__), "/county/#{state_code}.csv")
   end
 end
