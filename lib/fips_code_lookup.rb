5e6916dc263f519ff1455b5cb3cf627cd6fe6475
Erik Barroso <erik.barroso@primary.health>
2022-07-08 00:25:13 -0400

    testing first local build with al county state file

--- lib/fips_code_lookup.rb
+++ lib/fips_code_lookup.rb
@@ -4,5 +4,13 @@ require_relative "fips_code_lookup/version"
 
 module FipsCodeLookup
   class Error < StandardError; end
+
   # Your code goes here...
+  def county(state, name)
+    # lookup state code to file
+    # parse file for name and return fips code
+    csv = CSV.read("county/#{state}.csv", headers: true)
+    code_row = csv.find { |row| row["NAME"] == name }
+    puts "ROW: ", code_row
+  end
 end
