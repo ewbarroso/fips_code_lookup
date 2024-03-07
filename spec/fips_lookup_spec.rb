bcf5d1fc047cda1ba4b7c1003199b20d4b0e28c8
Erik <erikbarroso22@gmail.com>
2024-03-06 22:42:03 -0500

    adds specs for county lookup (worker method) and county method as memoization / attribute accessor

--- spec/fips_lookup_spec.rb
+++ spec/fips_lookup_spec.rb
@@ -5,7 +5,46 @@ RSpec.describe FipsLookup do
     expect(FipsLookup::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".county_lookup" do
+    context "with searchable state and county params" do
+      it "returns the proper fips code" do
+        expect(FipsLookup.county_lookup("Al", "Autauga County")).to eq("001")
+      end
+    end
+
+    context "with an invalid county param" do
+      context "and return_nil paramater is not used" do
+        it "returns an error" do
+          expect{FipsLookup.county_lookup("AL", "Autauga")}.to raise_error(StandardError, "No county found matching: Autauga")
+        end
+      end
+      context "and return_nil paramater is used" do
+        it "returns nil" do
+          expect(FipsLookup.county_lookup("Alabama", "Autauga", true)).to eq(nil)
+        end
+      end
+    end
+
+    context "with an invalid state param" do
+      context "and return_nil paramater is not used" do
+        it "returns an error" do
+          expect{FipsLookup.county_lookup("ZZ", "County")}.to raise_error(StandardError, "No state found matching: ZZ")
+        end
+      end
+      context "and return_nil paramater is used" do
+        it "returns nil" do
+          expect(FipsLookup.county_lookup("ZZ", "County", true)).to eq(nil)
+        end
+      end
+    end
+  end
+
+  describe ".county" do
+    it "populates a memoized hash attribute accessor with state paramater and county paramater as lookups" do
+      expect(FipsLookup.county("AL", "Autauga County")).to eq("001")
+
+      lookup = ["AL", "Autauga County"]
+      expect(FipsLookup.county_fips[lookup]).to eq("001")
+    end
   end
 end
